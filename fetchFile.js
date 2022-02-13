52eefc2cf932c9cd4ca1fe2c79825b92d1b81744
daviepineda1 <87446052+daviepineda1@users.noreply.github.com>
2022-02-12 17:13:07 -0700

    create fetchFilel functions

--- fetchFile.js
+++ fetchFile.js
@@ -1,1 +1,25 @@
-const firsy
+const first_result = document.getElementById('first_result');
+const second_result = document.getElementById('second_result');
+const first_btn = document.getElementById('first_btn');
+const second_btn = document.getElementById('second_btn');
+
+
+
+first_btn.addEventListener('click', getRandomPicture)
+second_btn.addEventListener('click', getRandomPicture2)
+
+function getRandomPicture(){
+    fetch('url')
+        .then(res => res.json())
+        .data(data => {
+            first_result.innerHTML = '<img src="${data.file}"/>'
+        })
+}
+
+function getRandomPicture2(){
+    fetch('url')
+        .then(res => res.json())
+        .data(data => {
+            first_result.innerHTML = '<img src="${data.file}"/>'
+        })
+}
